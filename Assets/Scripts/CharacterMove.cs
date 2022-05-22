c756f7e4837233048cb2077398a6b8544476438e
Burakhan Altay <burakhan.altay@gmail.com>
2022-05-22 22:47:39 +0300

    Movement and Camera

--- Assets/Scripts/CharacterMove.cs
+++ Assets/Scripts/CharacterMove.cs
@@ -1,18 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody rigidbodyOfPlayer;
+    private float speedOfPlayer;
+    private Vector3 playerForwardVector;
+   
+    private void Start()
     {
         
+        rigidbodyOfPlayer = GetComponent<Rigidbody>();
+        speedOfPlayer = 20f;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        playerForwardVector = new Vector3(Input.GetAxis("Horizontal"),0,1);
+        Debug.Log(playerForwardVector);
+        rigidbodyOfPlayer.MovePosition(transform.position + playerForwardVector * Time.deltaTime * speedOfPlayer);
     }
+    
 }
