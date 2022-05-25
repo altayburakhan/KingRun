be48f2d85af5c1a4caa86efca82f5ffedb4f0834
Burakhan Altay <burakhan.altay@gmail.com>
2022-05-25 19:36:57 +0300

    Map Updated

--- Assets/Scripts/CharacterMove.cs
+++ Assets/Scripts/CharacterMove.cs
@@ -9,36 +9,41 @@ public class CharacterMove : MonoBehaviour
     private Rigidbody rigidbodyOfPlayer; 
     private float speedOfPlayer = 20f;
     // Player`s Vector.
-    private Vector3 playerForwardVector;
-
-    //private Vector3 playersPosition;
+    private Vector3 playerForwardVector; 
+    private bool right, left;
+    private Vector3 playersPosition;
     // Touch Vectors.
     private Vector3 startTouchPosition;
     private Vector3 currentPosition;
     private Vector3 endTouchPosition;
     private bool stopTouch = false;
+    
     public float swipeRange;
-    public float tapRange;
-   
+       
     private void Start()
     {
         // Setting player`s Rigidbody and its speed.
         rigidbodyOfPlayer = GetComponent<Rigidbody>();
-        speedOfPlayer = 20f;
     }
 
     private void Update()
     {
-       
-        Swipe();
+        //Vector3 horizontalMove = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        Vector3 goRight = new Vector3(2.5f, transform.position.y, transform.position.z);
+        Vector3 goLeft = new Vector3(-2.5f, transform.position.y, transform.position.z);
+        //Swipe();
         playerForwardVector = new Vector3(0,0,1);
         //Debug.Log(playerForwardVector);
         rigidbodyOfPlayer.MovePosition(transform.position + playerForwardVector * Time.deltaTime * speedOfPlayer);
+        //transform.Translate(0,0,speedOfPlayer * Time.deltaTime);
+        
     }
 
     // Touch Method.
     public void Swipe()
     {
+        Vector2 left = new Vector2(-2.5f, transform.position.y);
+        Vector2 right = new Vector2(2.5f, transform.position.y);
         //playersPosition = transform.position;
         // If player touch screen FIRST-ONE time  
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
@@ -56,14 +61,16 @@ public class CharacterMove : MonoBehaviour
                 {
                     //Statement
                     Debug.Log("left");
-                    //playersPosition.x = -2.5f;
+                    //transform.position = Vector3.Lerp(playersPosition,left, 5 );
+                    transform.position = Vector3.Lerp(transform.position, left, 3f * Time.deltaTime);
                     stopTouch = true;
                 }
 
                 else if (Distance.x > swipeRange)
                 {
                     //Statement
-                    //playersPosition.x = 2.5f;
+                    //transform.position = Vector3.Lerp(playersPosition,right, 5 );
+                    transform.position = Vector3.Lerp(transform.position, right, 3f * Time.deltaTime);
                     Debug.Log("Right");
                     stopTouch = true;
                 }
@@ -76,10 +83,7 @@ public class CharacterMove : MonoBehaviour
             stopTouch = false;
             endTouchPosition = Input.GetTouch(0).position;
             Vector3 Distance = endTouchPosition - startTouchPosition;
-            /*if (Mathf.Abs(Distance.x) < tapRange && Mathf.Abs(Distance.y) < tapRange)
-            {
-                
-            }*/
+            
         }
         
     }
