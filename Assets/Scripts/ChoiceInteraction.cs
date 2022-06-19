599d77233d6aa0f6537e0ab93576b98307eb73f0
Burakhan Altay <burakhan.altay@gmail.com>
2022-06-19 20:25:36 +0300

    Stage

---
+++ Assets/Scripts/ChoiceInteraction.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChoiceInteraction : MonoBehaviour
+{
+    private CharacterMove characterMove;
+
+    private void Start()
+    {
+        
+    }
+}
