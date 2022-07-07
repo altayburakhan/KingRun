ae3756c0a9b2f0671236501bf2d9b7f07bfb2958
Burakhan Altay <burakhan.altay@gmail.com>
2022-07-08 00:57:26 +0300

    Collider

--- Assets/Scripts/ChoiceInteraction.cs
+++ Assets/Scripts/ChoiceInteraction.cs
@@ -1,14 +1,37 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class ChoiceInteraction : MonoBehaviour
 {
-    private CharacterMove characterMove;
-
     private void Start()
     {
         
+    }
+    private void Update()
+    {
+        
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.CompareTag("mage"))
+        {
+            Destroy(hit.gameObject);
+        }
+        if (hit.gameObject.CompareTag("barbarian"))
+        {
+            Destroy(hit.gameObject);
+        }
+        if (hit.gameObject.CompareTag("warrior"))
+        {
+            Destroy(hit.gameObject);
+        }
+        if (hit.gameObject.CompareTag("archer"))
+        {
+            Destroy(hit.gameObject);
+        }
     }
 }
